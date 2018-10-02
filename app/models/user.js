d425958ff43f660816a5a31875b7b754c3c87afa
Susan Yishan Zheng <yishanzh@usc.edu>
2018-10-02 13:05:54 -0700

    added last name

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
-  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
   jobTitle: DS.attr('string'),
   phoneNumber: DS.attr('string')
 });
